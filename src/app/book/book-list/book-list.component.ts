86dfdc408d33442f37cca3111f80c5cea8646477
jf.rubio <jf.rubio@uniandes.edu.co>
2019-12-12 17:04:37 -0500

    intento de descargar

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Book } from '../book';
 import { BookService } from '../book.service';
 
+const btnfilterByC1: HTMLElement = document.getElementById("button-filterByC1");
+btnfilterByC1.onclick = function() { applyFilterByC1();};
+
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
@@ -21,4 +24,19 @@ export class BookListComponent implements OnInit {
     console.log("getBooks on init");
     this.bookService.getBooks().subscribe(books => this.books = books);
   } 
+ 
 }
+
+function applyFilterByC1() {
+  clearCoursesInTable();
+  let booksfiltered: Book[] = searchCourseByC1(1, this.books);
+}
+ function clearCoursesInTable() {
+  while (this.books.hasChildNodes()) {
+    this.books.removeChild(this.books.lastChild);
+  }
+}
+  function searchCourseByC1(categoria: number, books: Book[]) {
+    let categoriaT:string = categoria+"";
+    return categoriaT === '' ? books : books.filter((course) => course.categoria==categoria);
+  }
